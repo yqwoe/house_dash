1e5537d095fe625ae1bf508a4d66a3ec2d0696cf
yqwoe <yuangzhangchn@gmail.com>
2020-04-26 00:56:39 +0800

    nginx open ssl

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :puma_init_active_record, false  # Change to tr
 append :linked_files, "config/database.yml","config/secrets.yml" 
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system","public/dist"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
