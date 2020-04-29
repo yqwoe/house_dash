0436282e333e601ab7e92800916b766dd13dc4d7
yqwoe <yuangzhangchn@gmail.com>
2020-04-30 02:24:08 +0800

    proxy pool

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,8 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
   mount Sidekiq::Web => '/sidekiq'
+  Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == ["yqwoe", "yqwoe900316"]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
