3033db886420e3b915e6b4cfb838a4f9636b4f49
yqwoe <yuangzhangchn@gmail.com>
2020-05-01 18:01:52 +0800

    proxy pools index api

--- app/serializers/proxy_pool_serializer.rb
+++ app/serializers/proxy_pool_serializer.rb
@@ -1,6 +1,6 @@
 class ProxyPoolSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :ip, :port,:active,:check_count,:fail_count,:protocol,:long_time
+  attributes :ip, :port,:active,:check_count,:fail_count,:protocol,:long_time,:updated_at
   cache_options enabled: true, cache_length: 12.hours
 end
