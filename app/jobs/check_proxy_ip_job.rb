19ac55a76af295e93a6dcff6338e222d471374a1
yqwoe <yuangzhangchn@gmail.com>
2020-05-02 15:07:25 +0800

    proxy pools index api

--- app/jobs/check_proxy_ip_job.rb
+++ app/jobs/check_proxy_ip_job.rb
@@ -7,42 +7,47 @@ def perform(id)
 
         return true if @proxy.nil?
       
-       long_time = time_of do 
-      begin
-        headers = {"User-Agent": "Mozilla/5.0"}
+       long_time =  0
 
-         resp = RestClient::Request.execute(method: :get, url: 'https://www.github.com', proxy: "#{@proxy.protocol.downcase}://#{@proxy.ip}:#{@proxy.port}",timeout: 3,headers: headers)
+        time = Time.now.to_i
+            begin
+              headers = {"User-Agent": "Mozilla/5.0"}
 
-        #  puts resp.body
-        if @proxy.fail_count.to_i > 0
-            @proxy.active = 1
-            @proxy.check_count = @proxy.check_count.to_i + 1
-            @proxy.fail_count = @proxy.fail_count.to_i - 1
-            @proxy.long_time = long_time
-        else
-          @proxy.active = 1
-            @proxy.check_count = @proxy.check_count.to_i + 1
-            @proxy.long_time = long_time
-        end
-      rescue => e
-        Rails.logger.error e
-            @proxy.active = 0
-            @proxy.check_count = @proxy.check_count.to_i + 1
-            @proxy.fail_count = @proxy.fail_count.to_i + 1
-            @proxy.long_time = long_time
-      end
+              resp = RestClient::Request.execute(method: :get, url: 'https://www.github.com', proxy: "#{@proxy.protocol.downcase}://#{@proxy.ip}:#{@proxy.port}",timeout: 3,headers: headers)
 
-       end
 
+              end_time = Time.now.to_i
+
+              long_time = (end_time - time) / 1000.00
+              #  puts resp.body
+              if @proxy.fail_count.to_i > 0
+                  @proxy.active = 1
+                  @proxy.check_count = @proxy.check_count.to_i + 1
+                  @proxy.fail_count = @proxy.fail_count.to_i - 1
+                  @proxy.long_time = long_time
+              else
+                @proxy.active = 1
+                  @proxy.check_count = @proxy.check_count.to_i + 1
+                  @proxy.long_time = long_time
+              end
+            rescue => e
+              Rails.logger.error e
+
+              end_time = Time.now.to_i
+
+              long_time = (end_time - time) / 1000.00
+                  @proxy.active = 0
+                  @proxy.check_count = @proxy.check_count.to_i + 1
+                  @proxy.fail_count = @proxy.fail_count.to_i + 1
+                  @proxy.long_time = long_time
+            end
       return @proxy.save!
     end
 
 
     def time_of &block
-      time = Time.now.to_i
       puts "before time #{time}"
         block.call
-      end_time = Time.now.to_i
 
       puts "end time #{end_time}"
 
